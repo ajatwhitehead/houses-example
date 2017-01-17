00cbc6137662f0c338a7ed77776cbe2dab630116
Aja Whitehead <aja.whitehead1@gmail.com>
2017-01-17 11:57:34 -0500

    initial commit

---
+++ HOUSES EXAMPLES/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HOUSES_EXAMPLES
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
